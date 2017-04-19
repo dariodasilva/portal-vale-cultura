0bf414f63386b6e152b60bb5b0fa562329b6dcc5
Ruy Ferreira <ruyjfs@gmail.com>
2017-04-19 18:55:59 -0300

    - Ajustes na api.

--- projeto/application/modules/api/controllers/IndexController.php
+++ projeto/application/modules/api/controllers/IndexController.php
@@ -11,15 +11,17 @@ class Api_IndexController extends ValeCultura_Controller_Rest_Abstract {
     }
 
     public function indexAction() {
-        echo Zend_Json::encode(array(
-            'disponiveis' => array(
-                'trabalhadores-acumulados',
-                'trabalhadores-acumulados-por-ano',
-                'trabalhadores-acumulados-por-localizacao',
-                'trabalhadores-ativos',
-                'trabalhadores-inativos',
+        $this->getResponse()->setBody(Zend_Json::encode(
+            array(
+                'disponiveis' => array(
+                    'trabalhadores-acumulados',
+                    'trabalhadores-acumulados-por-ano',
+                    'trabalhadores-acumulados-por-localizacao',
+                    'trabalhadores-ativos',
+                    'trabalhadores-inativos',
 //                'beneficiarios',
 //                'consumos',
+                )
             )
         ));
     }
@@ -29,53 +31,33 @@ class Api_IndexController extends ValeCultura_Controller_Rest_Abstract {
      * should respond with the server resource state of the resource identified
      * by the 'id' value.
      */
-    public function getAction()
-    {
-        $arrResult = [];
-        $arrResult[] = ['ano' => 2014, 'mes' => 'Janeiro', 'regiao' => 'Brasilia', 'estado' => 'DF', 'qtd_trabalhadores'=> 3054];
-        $arrResult[] = ['ano' => 2014, 'mes' => 'Janeiro', 'regiao' => 'Brasilia', 'estado' => 'SP', 'qtd_trabalhadores'=> 3054];
-        $arrResult[] = ['ano' => 2014, 'mes' => 'Janeiro', 'regiao' => 'Brasilia', 'estado' => 'MG', 'qtd_trabalhadores'=> 3054];
-        $arrResult[] = ['ano' => 2014, 'mes' => 'Janeiro', 'regiao' => 'Brasilia', 'estado' => 'GO', 'qtd_trabalhadores'=> 3054];
-        $arrResult[] = ['ano' => 2014, 'mes' => 'Janeiro', 'regiao' => 'Brasilia', 'estado' => 'MT', 'qtd_trabalhadores'=> 3054];
-//
-//        echo Zend_Json::encode($arrResult);
-        $this->getResponse()->setBody(Zend_Json::encode($arrResult));
-        $this->getResponse()->setHttpResponseCode(200);
-    }
+    public function getAction(){}
 
     /**
      * The head action handles HEAD requests and receives an 'id' parameter; it
      * should respond with the server resource state of the resource identified
      * by the 'id' value.
      */
-    public function headAction(){
-
-    }
+    public function headAction(){}
 
     /**
      * The post action handles POST requests; it should accept and digest a
      * POSTed resource representation and persist the resource state.
      */
-    public function postAction(){
-
-    }
+    public function postAction(){}
 
     /**
      * The put action handles PUT requests and receives an 'id' parameter; it
      * should update the server resource state of the resource identified by
      * the 'id' value.
      */
-    public function putAction(){
-
-    }
+    public function putAction(){}
 
     /**
      * The delete action handles DELETE requests and receives an 'id'
      * parameter; it should update the server resource state of the resource
      * identified by the 'id' value.
      */
-    public function deleteAction(){
-
-    }
+    public function deleteAction(){}
 }
 
