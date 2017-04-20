adcfe5a68877ae15ff15fd0ea00fd85e9d45a14d
Ruy Ferreira <ruyjfs@gmail.com>
2017-04-20 14:40:56 -0300

    - Ajustes na api.

--- projeto/application/modules/api/Bootstrap.php
+++ projeto/application/modules/api/Bootstrap.php
@@ -14,4 +14,22 @@ class Api_Bootstrap extends Zend_Application_Module_Bootstrap
 //         */
 //        $response->setHeader('Content-Type', 'text/html; charset=utf-8');
     }
+
+    public function _initRouteRest()
+    {
+//        $frontController = Zend_Controller_Front::getInstance();
+//        $restRoute = new Zend_Rest_Route(
+//            $frontController,
+//            array('module' => 'api'),
+//            array(
+//                'api' => array(
+//                    'trabalhadores-acumulados',
+//                    'trabalhadores-acumulados-por-ano',
+//                    'trabalhadores-acumulados-por-localizacao',
+//                    'trabalhadores-ativos',
+//                    'trabalhadores-inativos',
+//                )));
+//        $frontController->getRouter()->addRoute('rest', $restRoute);
+    }
 }
+
