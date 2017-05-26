b8f2baf06ae53ead7a10e6dce95b0b8a9e80dfde
Ruy Ferreira <ruyjfs@gmail.com>
2017-05-26 17:39:57 -0300

    - Modulo API: implementado tela inicial no formato html, informando os servicos que existem disponiveis e o tipo de requisicao delas.

--- projeto/application/modules/api/Bootstrap.php
+++ projeto/application/modules/api/Bootstrap.php
@@ -15,6 +15,16 @@ class Api_Bootstrap extends Zend_Application_Module_Bootstrap
 //        $response->setHeader('Content-Type', 'text/html; charset=utf-8');
     }
 
+    /**
+     * Implementa a rota para o servico rest apenas para o modulo api.
+     *
+     * @name _initRouteRest
+     *
+     * @author Ruy Junior Ferreira Silva <ruyjfs@gmail.com>
+     * @since  26/05/2017
+     *
+     * @todo verificar futuramente uma forma melhor de implementar a rota só no modulo api, por enquanto foi implementado a logica na mao.
+     */
     public function _initRouteRest()
     {
 //        $frontController = Zend_Controller_Front::getInstance();
@@ -30,6 +40,12 @@ class Api_Bootstrap extends Zend_Application_Module_Bootstrap
 //                    'trabalhadores-inativos',
 //                )));
 //        $frontController->getRouter()->addRoute('rest', $restRoute);
+
+        $frontController = Zend_Controller_Front::getInstance();
+        if (isset($_SERVER['REDIRECT_URL']) && is_int(strpos($_SERVER['REDIRECT_URL'], '/')) && explode('/', $_SERVER['REDIRECT_URL'])[1] == 'api') {
+            $router = $frontController->getRouter();
+            $router->addRoute('rest', new Zend_Rest_Route($frontController));
+        }
     }
 }
 
