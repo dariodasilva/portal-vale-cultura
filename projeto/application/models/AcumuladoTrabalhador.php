2363611087f845f81d93f013a2a9ffb38e931398
Fernão Lopes <fernao@riseup.net>
2017-05-29 15:30:06 -0300

    Novo: [Módulo API] alteração nas rotas - unificando todas rotas

--- projeto/application/models/AcumuladoTrabalhador.php
+++ projeto/application/models/AcumuladoTrabalhador.php
@@ -3,6 +3,7 @@
 class Application_Model_AcumuladoTrabalhador extends ValeCultura_Db_Table_Abstract {
 
     private $table = null;
+    private $name = 'ACUMULADO_TRABALHADOR';
     
     public function getTable() {
         if (is_null($this->table)) {
@@ -11,16 +12,51 @@ class Application_Model_AcumuladoTrabalhador extends ValeCultura_Db_Table_Abstra
         return $this->table;
     }
 
-    public function getPorData($ano = null, $mes = null) {
-        $where = array('TIPO = ?' => '2');
+    public function getTotal($tipo) {
+        $select = $this->getTable()->select();
+        
+        $select->from($this->name, new Zend_Db_Expr('DESCRICAO_1 AS titulo, DESCRICAO_2 AS tipo, valor'), 'BI_VALE_CULTURA');
 
+        if ($tipo == 'acumulados') {
+            $select->where('DESCRICAO_2 = ?', 'CADASTRADOS');
+        } else if ($tipo == 'ativos') {
+            $select->where('DESCRICAO_2 = ?', 'ATIVOS');
+        }
+        
+        return $this->getTable()->fetchAll($select)->toArray();
+    }
+    
+    public function getPorData($tipo = null, $ano = null, $mes = null) {
+        $select = $this->getTable()->select();
+
+        $codTipo = ($tipo == 'acumulados') ? 2 : 3;  // acumulados 2, ativos 3        
+        $select->where('TIPO = ?', $codTipo);
+        $select->from($this->name, new Zend_Db_Expr('DESCRICAO_1 AS ano, DESCRICAO_2 AS mes, ano'), 'BI_VALE_CULTURA');
+        
         if ($ano) {
-            $where['DESCRICAO_1 = ?'] = $ano;
+            $select->where('DESCRICAO_1 = ?', $ano);
         }
         if ($mes) {
-            $where['DESCRICAO_2 = ?'] = $mes;
+            $select->where('DESCRICAO_2 = ?', $mes);
+        }
+        
+        return $this->getTable()->fetchAll($select)->toArray();
+    }
+
+    public function getPorLocalizacao($regiao = null, $uf = null) {
+        $select = $this->getTable()->select();
+        
+        $select->where('TIPO = ?', '4');
+        $select->from($this->name, new Zend_Db_Expr('DESCRICAO_1 AS regiao, DESCRICAO_2 AS uf, valor'), 'BI_VALE_CULTURA');
+        
+        if ($regiao) {
+
+            $select->where('DESCRICAO_1 = ?', $regiao);
+        }
+        if ($uf) {
+            $select->where('DESCRICAO_2 = ?', $uf);
         }        
         
-        return $this->select($where);
-    }
+        return $this->getTable()->fetchAll($select)->toArray();
+    }    
 }
